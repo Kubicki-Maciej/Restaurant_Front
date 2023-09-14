c78273f5a27ca0bca2e66a9995238b7a0c98b685
Maciej Kubicki <dev.kubicki@gmail.com>
2023-09-14 12:42:26 +0200

    start working on ManagerInfoGrid

--- src/screens/ManagerScreen/ManagerCards/DashboardCard.jsx
+++ src/screens/ManagerScreen/ManagerCards/DashboardCard.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import ManagerBarGrid from "../MiniBoxes/ManagerBarGrid";
+import ManagerInfoGrid from "../MiniBoxes/ManagerInfoGrid";
+import ManagerTableGrid from "../MiniBoxes/ManagerTableGrid";
 
 export default function DashboardCard() {
-  return <div>DashboardCard</div>;
+  return (
+    <div className="Screen">
+      Screen
+      <ManagerBarGrid />
+      <ManagerInfoGrid />
+      <ManagerTableGrid />
+    </div>
+  );
 }
